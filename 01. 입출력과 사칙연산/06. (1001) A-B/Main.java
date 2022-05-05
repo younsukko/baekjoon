a12b2d15a17f65efcf0d6d3d24a8703e64ae3cc8
younsukko <koyounsuk0208@gmail.com>
2022-05-05 13:29:04 +0900

    javabaekjoon

---
+++ 01. 입출력과 사칙연산/06. (1001) A-B/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int A, B;
+
+		A = sc.nextInt();
+
+		B = sc.nextInt();
+
+		System.out.print(A - B);
+
+		sc.close();
+
+	}
+
+}
